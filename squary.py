525a27fcc3a21d071911016c1f078c31076c04c8
I Gusti Agung Premananda <premananda2003@gmail.com>
2023-08-27 19:26:39 +0700

    Modified the color of the square to blue

--- squary.py
+++ squary.py
@@ -3,7 +3,7 @@ turtle.bgcolor("black")
 
 square = turtle.Turtle()
 square.speed(20)
-square.pencolor("red")
+square.pencolor("blue")
 for i in range(400):
     square.forward(i)
     square.left(91)
